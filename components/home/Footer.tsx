af630d8ff972e4f46632d94a50c5acee0d58d686
Faith Oluwadamilare Oyewole <124932742+Faith4078@users.noreply.github.com>
2025-04-21 20:25:16 +0100

    Update Footer.tsx

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Faith Oluwadamilare Oyewole
+          Copyright © 2025 Faith Oluwadamilare Oyewole
         </p>
         <div className="mt-5 flex gap-4 items-center  lg:gap-6">
           {socialMedia.map((profile) => {
